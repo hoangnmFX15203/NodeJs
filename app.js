4fc93d1bbda423404b752b0659d547f112aded43
hoangnmFX15203 <hoangnmfx15203@funix.edu.vn>
2022-09-09 15:02:20 +0700

    LAB1.3

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 function rqListener (req, res) {
-    console.log(req)
+    console.log(req.url, req.method, req.headers)
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
+    res.write('</html>')
+    res.end()
 }
 
 const server = http.createServer(rqListener)
