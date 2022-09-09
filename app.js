4fc9c90c50c6f57bdbd5cf860da0c5cf533a305c
hoangnmFX15203 <hoangnmfx15203@funix.edu.vn>
2022-09-09 15:31:25 +0700

    LAB1.4

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 function rqListener (req, res) {
-    console.log(req.url, req.method, req.headers)
+    const url = req.url
+
+    if (url === "/") {
+        res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+}
+    
 
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
