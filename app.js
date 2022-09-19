d131755ccfae3137aa628879d845702fe477f359
hoangnmFX15203 <hoangnmfx15203@funix.edu.vn>
2022-09-19 14:54:55 +0700

    LAB2.1

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
 
-function rqListener (req, res) {
-    const url = req.url
-    const method = req.method
+app.use ('/users', (req, res) => {
+  res.send('<p>The Middleware that handles just /</p>')
+})
 
-    if (url === "/") {
-        res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
-    res.write('</html>')
-    return res.end()
-}
-    if (url === "/message" && method === "POST") {
-        const body = []
-        req.on('data', (chunk) => {
-            body.push(chunk)
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message)
-        })
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
-    }
-    
+app.use ('/', (req, res) => {
+  res.send('<p>The Middleware that handles just /users</p>')
+})
 
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
-    res.write('</html>')
-    res.end()
-}
-
-const server = http.createServer(rqListener)
-
-server.listen(3000)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ testapp.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+function rqListener (req, res) {
+    const url = req.url
+    const method = req.method
+
+    if (url === "/") {
+        res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+}
+    if (url === "/message" && method === "POST") {
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302
+            res.setHeader('Location', '/')
+            return res.end()
+            })
+            
+        })
+    }
+    
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
+    res.write('</html>')
+    res.end()
+}
+
+const server = http.createServer(rqListener)
+
+server.listen(3000)
